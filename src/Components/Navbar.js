b50242466e9e57d6b4eccbd91815d5a7c1bfac3a
Isbah Ahmed Feroz <64509937+isbahchem@users.noreply.github.com>
2023-08-08 15:10:23 +0500

    Project started

---
+++ src/Components/Navbar.js
@@ -0,0 +1,13 @@
+
+function Navbar(){
+    return(
+        <nav class="navbar navbar-light" Style={"backgroundColor: +#e3f2fd;"}>
+  <div class="container-fluid">
+    <span class="navbar-brand mb-1 mx-1 h2">Home</span>
+    <span class="navbar-brand mb-1 my-1 h2">About</span>
+  </div>
+</nav>
+    )
+}
+
+export default Navbar;
